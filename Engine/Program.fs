c2cb7c1c70b08db4500d5781866e7fcdfd994b85
jakecoltman <jakecoltman@sky.com>
2016-07-11 10:38:51 +0100

    Voting engine.
    Takes in a list of votes per user and aggregates preference

--- Engine/Program.fs
+++ Engine/Program.fs
@@ -1,7 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open Engine.Vote
 
 [<EntryPoint>]
 let main argv = 
+    
+    let Jake : User = "Jake"
+    let user_weights : UserWeights = Map.empty.Add(Jake, 1.0)
+    
+    let jake_vote = Jake, Engine.Vote.Opposed
+
+    printfn "%A" (execute_election user_weights [jake_vote])
+
+    System.Console.Read() |> ignore
     printfn "%A" argv
     0 // return an integer exit code
---
+++ Engine/Vote.fs
@@ -0,0 +1,30 @@
+﻿namespace Engine
+
+module Vote = 
+
+    type User = string
+
+    type UserWeights = Map<User, float>
+
+    type VoteDirection = 
+        | InFavour
+        | Opposed
+
+    type Vote = User * VoteDirection
+
+    let execute_election (weights: UserWeights) (votes : List<Vote>) = 
+        let summation = 
+            votes
+            |> List.map (fun (x : Vote) -> 
+                match x with
+                    | user, InFavour -> weights.[user]
+                    | user, Opposed -> - weights.[user]
+                )
+            |> List.reduce (fun x y -> x + y)
+
+        let result =
+             match summation with 
+                | i when i <= 0.0 -> Opposed
+                | _ -> InFavour
+
+        result
