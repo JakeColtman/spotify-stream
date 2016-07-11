2ea7451ae9f3e547aeac689cf2946c35229abf78
jakecoltman <jakecoltman@sky.com>
2016-07-11 10:11:45 +0100

    Super simple first implementation
    Can add songs and pull them out again.  Wrapper around queue

---
+++ Engine/PlayQueue.fs
@@ -0,0 +1,19 @@
+﻿namespace Engine 
+
+module PlayQueue = 
+
+    open Engine.Music
+    open System.Collections
+
+    type PlayQueue = {
+        queue: Queue
+    };
+
+    let add_song (queue : PlayQueue) (song : Song) = 
+        queue.queue.Enqueue(song)
+        queue
+
+    let get_next_song (queue : PlayQueue) = 
+        let song = queue.queue.Dequeue
+        song, queue
+
